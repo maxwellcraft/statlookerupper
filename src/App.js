8d3e6a8bc5ef58a7291913540c40573c258414ea
Maxwell <mac5xb@virginia.edu>
2022-05-27 21:04:23 +0100

    init backend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import DoSomething from './QueryResults';
 
 // import ReactDOM from "react-dom";
+// setting up a backend data model
 
 function MyForm() {
   const [textarea, setQueryText] = useState('');
